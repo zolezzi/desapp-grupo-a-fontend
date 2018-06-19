7cc864136d3ede78e347545185e39108ea370935
carlos.cardozo <carlos.cardozo@artdecode.com.ar>
2018-06-19 16:56:57 -0300

    add module home

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,28 @@
+
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA,Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+import { ReactiveFormsModule,FormBuilder } from '@angular/forms';
+import * as services from '../shared/services/index';
+import { ComponentsModule } from '../components/components.module';
+import { HomeComponent } from './index';
+
+
+@NgModule({
+    imports: [ReactiveFormsModule,CommonModule, SharedModule, ComponentsModule],
+    declarations: [HomeComponent],
+    exports: [HomeComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class HomeModule {
+
+  constructor( @Optional() @SkipSelf() parentModule:HomeModule) {
+    if (parentModule) {
+      throw new Error('HomeModule is already loaded. ');
+    }
+
+    console.log('Load HomeModule');
+  }
+
+}
