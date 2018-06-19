ac155d4ed143b771eb3c97255c29cf9063f0d08a
zolezzi <cezcardozo23@gmail.com>
2018-06-19 17:09:37 -0300

    add dashboard example

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,28 @@
+
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA,Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
+import { ReactiveFormsModule,FormBuilder } from '@angular/forms';
+import * as services from '../shared/services/index';
+// import { ComponentsModule } from '../components/components.module';
+import { DashboardComponent } from './index';
+
+
+@NgModule({
+    imports: [ReactiveFormsModule,CommonModule, SharedModule],
+    declarations: [DashboardComponent],
+    exports: [DashboardComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class DashboardModule {
+
+  constructor( @Optional() @SkipSelf() parentModule:DashboardModule) {
+    if (parentModule) {
+      throw new Error('DashboardModule is already loaded. ');
+    }
+
+    console.log('Load DashboardModule');
+  }
+
+}
---
+++ src/app/dashboard/dashboard.route.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+import { DashboardComponent } from './index';
+
+export const DashboardRoutes: Route[] = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+
+];
