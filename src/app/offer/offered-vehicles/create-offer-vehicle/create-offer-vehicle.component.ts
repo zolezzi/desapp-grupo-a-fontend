291918fdd4d7481207b1bae93b1132f4ae5bc428
zolezzi <cezcardozo23@gmail.com>
2018-07-09 22:48:47 -0300

    Add refactor in create offer and in list offers

--- src/app/offer/offered-vehicles/create-offer-vehicle/create-offer-vehicle.component.ts
+++ src/app/offer/offered-vehicles/create-offer-vehicle/create-offer-vehicle.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MapsComponent }  from '../../../maps/maps.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserResourceApiService } from '../../../shared/services/user-resource-api.service';
+import { VehicleResourceApiService } from '../../../shared/services/vehicle-resource-api.service';
+import { PublicationResourceApiService } from '../../../shared/services/publication-resource-api.service';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-create-offer-vehicle',
@@ -10,16 +16,69 @@ export class CreateOfferVehicleComponent implements OnInit {
   images: Array<string> = [];
   entity: any = {};
   imageSelected: string = '';
+  vehicle: any = {};
+  vehicles: Array<any> = [];
+  router: any;
+  userCurrent:any;
+  types: Array<any> = [];
+  disabled: boolean = true;
 
-  constructor() { }
+  constructor(protected route: ActivatedRoute, router:Router, protected userResourceApiService:UserResourceApiService,
+    protected vehicleResourceApiService:VehicleResourceApiService, protected publicationResourceApiService:PublicationResourceApiService,
+    protected localStorageService:LocalStorageService) {
+
+      this.router = route;
+  }
 
   ngOnInit() {
+
+    this.images = [];
+
+    var vehicleId = this.router.snapshot.params["id"];
+
+    console.log("ID:", vehicleId);
+
+    this.userCurrent = this.localStorageService.retrieve('userCurrent');
+
+    this.entity.userOfferentId = this.userCurrent.id;
+
+    this.vehicleResourceApiService.searchAllTypeVehicle().subscribe(result => {
+      console.log("All types:", result);
+      this.types = result;
+    });
+
+    this.vehicleResourceApiService.allMyVehiclesRegitered(this.userCurrent.id).subscribe(result => {
+      this.vehicles = result;
+
+      if(vehicleId != undefined){
+        this.entity.vehicleId = Number(vehicleId);
+        this.vehicle = this.vehicles.filter(vehicle => vehicle.id === Number(vehicleId))[0];
+        console.log(this.vehicle);
+      }
+
+    });
 
   }
 
   addImage(){
     this.images.push(this.imageSelected);
     this.imageSelected = '';
+  }
+
+  createPublication(){
+    console.log("*****CREATE PUBLICATION*****");
+
+    this.entity.photos = this.images.filter(value => value != '');
+
+    var start = this.entity.startingDate;
+    var end = this.entity.endingDate
+
+    this.entity.startingDate = new Date(start);
+    this.entity.endingDate = new Date(end);
+
+    this.publicationResourceApiService.setPublication(this.entity).subscribe(result => {
+      console.log(result);
+    });
   }
 
 }
--- src/app/offer/offered-vehicles/list-offered-vehicles/list-offered-vehicles.component.ts
+++ src/app/offer/offered-vehicles/list-offered-vehicles/list-offered-vehicles.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserResourceApiService } from '../../../shared/services/user-resource-api.service';
 import { VehicleResourceApiService } from '../../../shared/services/vehicle-resource-api.service';
+import { PublicationResourceApiService } from '../../../shared/services/publication-resource-api.service';
+import { LocalStorageService } from 'ngx-webstorage';
 import { VehicleFilter } from '../../../model/models';
 
 @Component({
@@ -12,18 +14,30 @@ import { VehicleFilter } from '../../../model/models';
 export class ListOfferedVehiclesComponent implements OnInit {
 
   router:any;
+  publications: Array<any> = [];
+  userCurrent:any = {};
 
   constructor(protected route: ActivatedRoute, router:Router, protected userResourceApiService:UserResourceApiService,
-    protected vehicleResourceApiService:VehicleResourceApiService) {
+    protected vehicleResourceApiService:VehicleResourceApiService, protected publicationResourceApiService:PublicationResourceApiService,
+    protected localStorageService:LocalStorageService) {
 
     this.router = router;
   }
 
   ngOnInit() {
+
+    this.userCurrent = this.localStorageService.retrieve('userCurrent');
+
+    this.publicationResourceApiService.getUserPublications(this.userCurrent.id).subscribe(result => {
+      console.log(result);
+      this.publications = result;
+
+    });
+
   }
 
   offerVehicle(){
-    this.router.navigate(["offers/register-vehicle"]);
+    this.router.navigate(["offers/publish-vehicle/new"]);
   }
 
 }
