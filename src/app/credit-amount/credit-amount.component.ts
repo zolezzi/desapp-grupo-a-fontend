7a10028de3a9c27318bd1d40e8287f4428ac939a
zolezzi <cezcardozo23@gmail.com>
2018-08-05 13:05:40 -0300

    Add credit amount

--- src/app/credit-amount/credit-amount.component.ts
+++ src/app/credit-amount/credit-amount.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'ngx-webstorage';
+import { User } from '../model/User';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CreditAmountResourceApiService } from '../shared/services/credit-amount-resource-api.service';
 
 @Component({
   selector: 'app-credit-amount',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreditAmountComponent implements OnInit {
 
-  constructor() { }
+  userCurrent:any = {};
+  router:any
+  entity:any = {};
+  amount:number;
+
+  constructor(protected route: ActivatedRoute, router:Router, protected creditAmountResourceApiService:CreditAmountResourceApiService,
+    protected localStorageService:LocalStorageService) {
+
+      this.router = router;
+  }
 
   ngOnInit() {
+
+    this.userCurrent = this.localStorageService.retrieve('userCurrent');
+
+
+  }
+
+  addCredit(){
+
+    var id = this.userCurrent.id;
+
+    this.creditAmountResourceApiService.addCredit(id, this.amount).subscribe(result => {
+      this.localStorageService.store('userCurrent', result);
+      this.amount = null;
+      
+    });
   }
 
 }
